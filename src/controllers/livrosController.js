22985e7ba7ce4e728d70983f78922415d119c59d
Lenon Ramos <lenon.br@hotmail.com>
2025-04-07 17:56:27 -0300

    filtro de busca dinamico

--- src/controllers/livrosController.js
+++ src/controllers/livrosController.js
@@ -68,9 +68,11 @@ class LivroController {
     try {
       const {editora, titulo } = req.query;
 
+      // const regex = new RegExp(titulo, "i");
+
       const busca = {};
       if(editora) busca.editora = editora;
-      if(titulo) busca.titulo = titulo
+      if(titulo) busca.titulo = { $regex: titulo, $options: "i"}; //ou regex;
 
       const livrosResultado = await livros.find(busca);
 
